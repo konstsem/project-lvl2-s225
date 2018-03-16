cb97291119cc416cdaba7785d5123152aa2fcc35
konstsem <konstantin.semenov.2@gmail.com>
2018-03-16 22:12:40 +0300

    step 5 (fix buildNode, added updated type)

--- src/index.js
+++ src/index.js
@@ -11,58 +11,49 @@ const parsers = {
   '.ini': ini.parse,
 };
 
-// const isObject = value => value instanceof Object;
-
-function Node(key, type, value = '', children = []) {
-  this.key = key;
-  this.type = type;
-  this.value = value;
-  this.children = children;
-}
-
 const typesOfNode = [
   {
     type: 'nested',
     check: (before, after) => _.isObject(before) && _.isObject(after) &&
       !(before instanceof Array && after instanceof Array),
-    buildNode: (key, type, before, after, func) => {
-      const node = new Node(key, type, '', func(before, after));
-      return node;
-    },
+    buildNode: (key, type, before, after, func) =>
+      ({ key, type: 'nested', children: func(before, after) }),
   },
   {
     type: 'inserted',
     check: (before, after) => !before && after,
-    buildNode: (key, type, before, after) => new Node(key, type, after),
+    buildNode: (key, type, before, after) =>
+      ({ key, type: 'inserted', value: after }),
   },
   {
     type: 'deleted',
     check: (before, after) => before && !after,
-    buildNode: (key, type, before) => new Node(key, type, before),
+    buildNode: (key, type, before) =>
+      ({ key, type: 'deleted', value: before }),
   },
   {
-    type: 'changed',
+    type: 'updated',
     check: (before, after) => before !== after,
     buildNode: (key, type, before, after) =>
-      [new Node(key, 'deleted', before), new Node(key, 'inserted', after)],
+      ({ key, type: 'updated', value: { before, after } }),
   },
   {
     type: 'unchanged',
     check: (before, after) => before === after,
-    buildNode: (key, type, before) => new Node(key, type, before),
+    buildNode: (key, type, before) =>
+      ({ key, type: 'unchanged', value: before }),
   },
 ];
 
 const buildAst = (objectBefore, objectAfter) => {
   const keys = _.union(Object.keys(objectBefore), Object.keys(objectAfter));
-  return _.flatten(keys.map((key) => {
+  return keys.map((key) => {
     const nodeBefore = objectBefore[key];
     const nodeAfter = objectAfter[key];
     const { type, buildNode } = _.find(typesOfNode, ({ check }) =>
       check(nodeBefore, nodeAfter));
-    const node = buildNode(key, type, nodeBefore, nodeAfter, buildAst);
-    return node;
-  }));
+    return buildNode(key, type, nodeBefore, nodeAfter, buildAst);
+  });
 };
 
 const renderAst = (ast, breaks, format = 'default') =>
--- src/renderers.js
+++ src/renderers.js
@@ -18,16 +18,38 @@ const renderObj = (obj, breaks) => {
   return `{\n${result}\n${' '.repeat(breaks - 2)}}`;
 };
 
+const defaultRenderers = [
+  {
+    check: type => type === 'nested',
+    renderNode: (node, breaks, renderAst) =>
+      `${' '.repeat(breaks)}${getPrefix(node.type)}${node.key}: ${renderAst(node.children, breaks + 4)}`,
+  },
+  {
+    check: type => type === 'inserted' || type === 'deleted' || type === 'unchanged',
+    renderNode: (node, breaks) => {
+      const valueAsStr = (_.isObject(node.value) && !_.isArray(node.value)) ?
+        renderObj(node.value, breaks + 4) : node.value;
+      return `${' '.repeat(breaks)}${getPrefix(node.type)}${node.key}: ${valueAsStr}`;
+    },
+  },
+  {
+    check: type => type === 'updated',
+    renderNode: (node, breaks) => {
+      const beforeAsStr = (_.isObject(node.value.before) && !_.isArray(node.value.before)) ?
+        renderObj(node.value.before, breaks + 4) : node.value.before;
+      const afterAsStr = (_.isObject(node.value.after) && !_.isArray(node.value.after)) ?
+        renderObj(node.value.after, breaks + 4) : node.value.after;
+      return [`${' '.repeat(breaks)}${getPrefix('deleted')}${node.key}: ${beforeAsStr}`,
+        `${' '.repeat(breaks)}${getPrefix('inserted')}${node.key}: ${afterAsStr}`].join('\n');
+    },
+  },
+];
+
 export default {
   default: (ast, breaks, renderAst) => {
     const astAsString = ast.map((node) => {
-      const prefix = getPrefix(node.type);
-      if (node.type === 'nested') {
-        return `${' '.repeat(breaks)}${prefix}${node.key}: ${renderAst(node.children, breaks + 4)}`;
-      }
-      const valueAsStr = (_.isObject(node.value) && !_.isArray(node.value)) ?
-        renderObj(node.value, breaks + 4) : node.value;
-      return `${' '.repeat(breaks)}${prefix}${node.key}: ${valueAsStr}`;
+      const { renderNode } = _.find(defaultRenderers, ({ check }) => check(node.type));
+      return renderNode(node, breaks, renderAst);
     }).join('\n');
     return `{\n${astAsString}\n${' '.repeat(breaks - 2)}}`;
   },
